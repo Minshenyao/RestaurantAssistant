03b11db891a7f209f137d4c5b9f8534f59eb9187
Minshenyao <minshenyao@huangpeideMacBook-Air.local>
2024-10-18 10:47:51 +0800

    添加了用户充值功能和用户自动注册功能，另外增加了一个新的用户表

--- README.md
+++ README.md
@@ -16,4 +16,8 @@ Go语言编译器版本、使用框架及其数据库版本`go1.23.1 + gin v1.10
 ### 数据库部分
 创建数据库: `create database Canteen;`
 
-创建表: `create table special_dishes (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(100) NOT NULL, quantily INT NOT NULL, price DECIMAL(10,2) NOT NULL, date DATE NOT NULL);`
+创建特殊菜品表: `create table special_dishes (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(100) NOT NULL, quantily INT NOT NULL, price DECIMAL(10,2) NOT NULL, date DATE NOT NULL);`
+
+创建用户信息表: `CREATE TABLE User (phone VARCHAR(11) NOT NULL PRIMARY KEY, vip TINYINT(1) NOT NULL DEFAULT 0, deposit DECIMAL(10, 2) NOT NULL DEFAULT 0);`
+
+
--- main.go
+++ main.go
@@ -7,6 +7,7 @@ package main
 import (
 	"RestaurantAssistant/Database"
 	"database/sql"
+	"errors"
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"log"
@@ -34,13 +35,17 @@ type Totals struct {
 	BillAmounts []BillAmount
 	Total       float64 `json:"total"`
 }
+type User struct {
+	Phone   string  `json:"phone"`
+	Vip     bool    `json:"vip"`
+	Deposit float64 `json:"deposit"`
+}
 
 func main() {
 	router := gin.Default()
 	db, err := Database.ConnectDB()
 	if err != nil {
 		log.Fatal(err)
-		return
 	}
 	router.GET("/todaySpecials", func(context *gin.Context) {
 		specials, err := getTodaySpecials(db)
@@ -101,6 +106,18 @@ func main() {
 		}
 		context.JSON(http.StatusOK, gin.H{"status": status})
 	})
+	router.POST("/memberRecharge", func(context *gin.Context) {
+		var userInfo User
+		if err := context.BindJSON(&userInfo); err != nil {
+			context.JSON(http.StatusInternalServerError, gin.H{"error": err})
+			return
+		}
+		err := memberRecharge(userInfo.Phone, userInfo.Deposit, db)
+		if err != nil {
+			context.JSON(http.StatusInternalServerError, gin.H{"error": err})
+		}
+		context.JSON(http.StatusOK, gin.H{"status": userInfo.Phone + "充值成功!"})
+	})
 	router.Run(":8000")
 }
 
@@ -141,14 +158,12 @@ func getTodaySpecials(db *sql.DB) ([]SpecialDish, error) {
 	rows, err := db.Query("select name, quantily, price from Special_dishes where date = ?", today)
 	if err != nil {
 		log.Fatal(err)
-		return nil, err
 	}
 	defer rows.Close()
 	for rows.Next() {
 		var special SpecialDish
 		if err := rows.Scan(&special.Name, &special.Number, &special.Price); err != nil {
 			log.Fatal(err)
-			return nil, err
 		}
 		specials = append(specials, special)
 	}
@@ -162,14 +177,12 @@ func billAmount(db *sql.DB) (Totals, error) {
 	rows, err := db.Query("select name, quantily, price from Special_dishes where date = ?", time.Now().Format("2006-01-02"))
 	if err != nil {
 		log.Fatal(err)
-		return Totals{}, err
 	}
 	defer rows.Close()
 	for rows.Next() {
 		var special SpecialDish
 		if err := rows.Scan(&special.Name, &special.Number, &special.Price); err != nil {
 			log.Fatal(err)
-			return Totals{}, err
 		}
 		var billAmount BillAmount
 		fmt.Printf("菜名: %s\t\t价格: %f\n", special.Name, special.Price*float64(special.Number))
@@ -183,4 +196,84 @@ func billAmount(db *sql.DB) (Totals, error) {
 	totals.BillAmounts = billAmounts
 	totals.Total = total
 	return totals, nil
+}
+
+// 会员充值系统
+func memberRecharge(phoneNumber string, money float64, db *sql.DB) error {
+	_ = register(phoneNumber, db)
+	userInfo, err := db.Query("select * from User where phone = ?", phoneNumber)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer userInfo.Close()
+	if money > 0.00 && money > 100.00 {
+		err := updateUserInfo(phoneNumber, money, 0.00, db)
+		if err != nil {
+			log.Fatal(err)
+		}
+	} else if money >= 100.00 && money < 200.00 {
+		err := updateUserInfo(phoneNumber, money, 10.00, db)
+		if err != nil {
+			log.Fatal(err)
+		}
+	} else if money >= 200 && money < 500 {
+		err := updateUserInfo(phoneNumber, money, 30.00, db)
+		if err != nil {
+			log.Fatal(err)
+		}
+	} else if money >= 500 && money < 1000 {
+		err := updateUserInfo(phoneNumber, money, 80.00, db)
+		if err != nil {
+			log.Fatal(err)
+		}
+	} else if money >= 1000 {
+		err := updateUserInfo(phoneNumber, money, 200.00, db)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	return nil
+}
+
+// updateUserInfo 更新用户充值信息
+func updateUserInfo(phoneNumber string, money float64, addition float64, db *sql.DB) error {
+	var vipStatus bool
+	var rawMoney float64
+	_ = register(phoneNumber, db)
+	err := db.QueryRow("SELECT vip, deposit FROM User WHERE phone = ?", phoneNumber).Scan(&vipStatus, &rawMoney)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if vipStatus == false && money >= 2000 {
+		vipStatus = true
+	} else {
+		time.Sleep(1 * time.Second) // 开个小玩笑
+	}
+	if vipStatus == false {
+		addition = 0.00
+	}
+	money += rawMoney
+	_, err = db.Exec("update User set deposit=?, vip=? where phone = ? ", money+addition, vipStatus, phoneNumber)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	return nil
+}
+
+// 注册用户
+func register(phoneNumber string, db *sql.DB) error {
+	var existingPhone string
+	err := db.QueryRow("SELECT phone FROM User WHERE phone = ?", phoneNumber).Scan(&existingPhone)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			_, err := db.Exec("INSERT INTO User (phone) VALUES (?)", phoneNumber)
+			if err != nil {
+				return err
+			}
+			return nil
+		}
+		return err
+	}
+	return errors.New("用户已经存在")
 }
